cb7950d49b8c2cf291838e0416f4b4fc734a5772
Vignesh S <vigneshs6993@gmail.com>
2022-06-13 11:07:37 +0530

    Nodejs limit memory usage and uncommented ll events

--- index.js
+++ index.js
@@ -18,355 +18,355 @@ const eventJson = [
     },
   },
 
-  // // Shadow lottery
-  // {
-  //   cron: "30 7 * * 1-6",
-  //   msg: {
-  //     title: "Shadow Lottery",
-  //     desc: "Shadow lottery is up. Apply for Lottery to get Akiba's signets",
-  //     color: "#A652BB",
-  //   },
-  // },
-  // {
-  //   cron: "0 30 13 * * 1-6",
-  //   msg: {
-  //     title: "Shadow Lottery",
-  //     desc: "Shadow lottery is up. Apply for Lottery to get Akiba's signets",
-  //     color: "#A652BB",
-  //   },
-  // },
-  // {
-  //   cron: "0 30 16 * * 1-6",
-  //   msg: {
-  //     title: "Shadow Lottery",
-  //     desc: "Shadow lottery is up. Apply for Lottery to get Akiba's signets",
-  //     color: "#A652BB",
-  //   },
-  // },
+  // Shadow lottery
+  {
+    cron: "30 7 * * 1-6",
+    msg: {
+      title: "Shadow Lottery",
+      desc: "Shadow lottery is up. Apply for Lottery to get Akiba's signets",
+      color: "#A652BB",
+    },
+  },
+  {
+    cron: "30 13 * * 1-6",
+    msg: {
+      title: "Shadow Lottery",
+      desc: "Shadow lottery is up. Apply for Lottery to get Akiba's signets",
+      color: "#A652BB",
+    },
+  },
+  {
+    cron: "30 16 * * 1-6",
+    msg: {
+      title: "Shadow Lottery",
+      desc: "Shadow lottery is up. Apply for Lottery to get Akiba's signets",
+      color: "#A652BB",
+    },
+  },
 
-  // // Shadow contracts
-  // {
-  //   cron: "0 30 22 * * 1-6",
-  //   msg: {
-  //     title: "Shadow Contracts",
-  //     desc: "1 Shadow Contract is reset. Do your Contract.",
-  //     color: "#A652BB",
-  //   },
-  // },
-  // {
-  //   cron: "0 30 22 * * 7",
-  //   msg: {
-  //     title: "Shadow Contracts",
-  //     desc: "3 Shadow Contracts are reset. Do your Contracts.",
-  //     color: "#A652BB",
-  //   },
-  // },
+  // Shadow contracts
+  {
+    cron: "30 22 * * 1-6",
+    msg: {
+      title: "Shadow Contracts",
+      desc: "1 Shadow Contract is reset. Do your Contract.",
+      color: "#A652BB",
+    },
+  },
+  {
+    cron: "30 22 * * 7",
+    msg: {
+      title: "Shadow Contracts",
+      desc: "3 Shadow Contracts are reset. Do your Contracts.",
+      color: "#A652BB",
+    },
+  },
 
-  // // Vault raid
-  // {
-  //   cron: "0 30 7 * * 1-7",
-  //   msg: {
-  //     title: "Vault Raid",
-  //     desc: "Immortal's Vault is open. Lets go for their Vault raid.",
-  //     color: "#A652BB",
-  //   },
-  // },
-  // {
-  //   cron: "0 30 14 * * 1-7",
-  //   msg: {
-  //     title: "Vault Raid",
-  //     desc: "Immortal's Vault is open. Lets go for their Vault raid.",
-  //     color: "#A652BB",
-  //   },
-  // },
+  // Vault raid
+  {
+    cron: "30 7 * * 1-7",
+    msg: {
+      title: "Vault Raid",
+      desc: "Immortal's Vault is open. Lets go for their Vault raid.",
+      color: "#A652BB",
+    },
+  },
+  {
+    cron: "30 14 * * 1-7",
+    msg: {
+      title: "Vault Raid",
+      desc: "Immortal's Vault is open. Lets go for their Vault raid.",
+      color: "#A652BB",
+    },
+  },
 
-  // // Shadow assembly
-  // {
-  //   cron: "0 30 13 * * 1-6",
-  //   msg: {
-  //     title: "Shadow Assembly",
-  //     desc: "Shadow Assembly is commencing now. Everyone get your Akiba's blessings. And your 15 Aspirant keys.",
-  //     color: "#A652BB",
-  //   },
-  // },
+  // Shadow assembly
+  {
+    cron: "30 13 * * 1-6",
+    msg: {
+      title: "Shadow Assembly",
+      desc: "Shadow Assembly is commencing now. Everyone get your Akiba's blessings. And your 15 Aspirant keys.",
+      color: "#A652BB",
+    },
+  },
 
-  // // Rite of exile
-  // {
-  //   cron: "0 15 15 * * 7",
-  //   msg: {
-  //     title: "Rite of Exile",
-  //     desc: "Rite of exile is commencing now in 15 mins. Need all hands on Deck needed.",
-  //     color: "#A652BB",
-  //   },
-  // },
-  // {
-  //   cron: "0 20 15 * * 7",
-  //   msg: {
-  //     title: "Rite of Exile",
-  //     desc: "Rite of exile is commencing now in 10 mins. Need all hands on Deck needed.",
-  //     color: "#A652BB",
-  //   },
-  // },
-  // {
-  //   cron: "0 25 15 * * 7",
-  //   msg: {
-  //     title: "Rite of Exile",
-  //     desc: "Rite of exile is commencing now in 5 mins. Need all hands on Deck needed.",
-  //     color: "#A652BB",
-  //   },
-  // },
-  // {
-  //   cron: "0 30 15 * * 7",
-  //   msg: {
-  //     title: "Rite of Exile",
-  //     desc: "Rite of exile is commencing now. Good luck bois.",
-  //     color: "#A652BB",
-  //   },
-  // },
+  // Rite of exile
+  {
+    cron: "15 15 * * 7",
+    msg: {
+      title: "Rite of Exile",
+      desc: "Rite of exile is commencing now in 15 mins. Need all hands on Deck needed.",
+      color: "#A652BB",
+    },
+  },
+  {
+    cron: "20 15 * * 7",
+    msg: {
+      title: "Rite of Exile",
+      desc: "Rite of exile is commencing now in 10 mins. Need all hands on Deck needed.",
+      color: "#A652BB",
+    },
+  },
+  {
+    cron: "25 15 * * 7",
+    msg: {
+      title: "Rite of Exile",
+      desc: "Rite of exile is commencing now in 5 mins. Need all hands on Deck needed.",
+      color: "#A652BB",
+    },
+  },
+  {
+    cron: "30 15 * * 7",
+    msg: {
+      title: "Rite of Exile",
+      desc: "Rite of exile is commencing now. Good luck bois.",
+      color: "#A652BB",
+    },
+  },
 
-  // // Shadow War
-  // {
-  //   cron: "0 15 13 * * 4,6",
-  //   msg: {
-  //     title: "Shadow War",
-  //     desc: "Shadow War is commencing now in 15 mins. Need all hands on Deck needed.",
-  //     color: "#A652BB",
-  //   },
-  // },
-  // {
-  //   cron: "0 20 13 * * 4,6",
-  //   msg: {
-  //     title: "Shadow War",
-  //     desc: "Shadow War is commencing now in 10 mins. Need all hands on Deck needed.",
-  //     color: "#A652BB",
-  //   },
-  // },
-  // {
-  //   cron: "0 25 13 * * 4,6",
-  //   msg: {
-  //     title: "Shadow War",
-  //     desc: "Shadow War is commencing now in 5 mins. Need all hands on Deck needed.",
-  //     color: "#A652BB",
-  //   },
-  // },
-  // {
-  //   cron: "0 30 13 * * 4,6",
-  //   msg: {
-  //     title: "Shadow War",
-  //     desc: "Shadow War is commencing now. Good luck bois.",
-  //     color: "#A652BB",
-  //   },
-  // },
+  // Shadow War
+  {
+    cron: "15 13 * * 4,6",
+    msg: {
+      title: "Shadow War",
+      desc: "Shadow War is commencing now in 15 mins. Need all hands on Deck needed.",
+      color: "#A652BB",
+    },
+  },
+  {
+    cron: "20 13 * * 4,6",
+    msg: {
+      title: "Shadow War",
+      desc: "Shadow War is commencing now in 10 mins. Need all hands on Deck needed.",
+      color: "#A652BB",
+    },
+  },
+  {
+    cron: "25 13 * * 4,6",
+    msg: {
+      title: "Shadow War",
+      desc: "Shadow War is commencing now in 5 mins. Need all hands on Deck needed.",
+      color: "#A652BB",
+    },
+  },
+  {
+    cron: "30 13 * * 4,6",
+    msg: {
+      title: "Shadow War",
+      desc: "Shadow War is commencing now. Good luck bois.",
+      color: "#A652BB",
+    },
+  },
 
-  // // Battlegrounds
-  // {
-  //   cron: "0 30 3 * * 1-7",
-  //   msg: {
-  //     title: "Battlegrounds",
-  //     desc: "Battlegrounds is starting now. Do your daily 3 battlegrounds to get rewards.",
-  //     color: "#F8C300",
-  //   },
-  // },
-  // {
-  //   cron: "0 30 7 * * 1-7",
-  //   msg: {
-  //     title: "Battlegrounds",
-  //     desc: "Battlegrounds is starting now. Do your daily 3 battlegrounds to get rewards.",
-  //     color: "#F8C300",
-  //   },
-  // },
-  // {
-  //   cron: "0 30 13 * * 1-7",
-  //   msg: {
-  //     title: "Battlegrounds",
-  //     desc: "Battlegrounds is starting now. Do your daily 3 battlegrounds to get rewards.",
-  //     color: "#F8C300",
-  //   },
-  // },
-  // {
-  //   cron: "0 30 17 * * 1-7",
-  //   msg: {
-  //     title: "Battlegrounds",
-  //     desc: "Battlegrounds is starting now. Do your daily 3 battlegrounds to get rewards.",
-  //     color: "#F8C300",
-  //   },
-  // },
+  // Battlegrounds
+  {
+    cron: "30 3 * * 1-7",
+    msg: {
+      title: "Battlegrounds",
+      desc: "Battlegrounds is starting now. Do your daily 3 battlegrounds to get rewards.",
+      color: "#F8C300",
+    },
+  },
+  {
+    cron: "30 7 * * 1-7",
+    msg: {
+      title: "Battlegrounds",
+      desc: "Battlegrounds is starting now. Do your daily 3 battlegrounds to get rewards.",
+      color: "#F8C300",
+    },
+  },
+  {
+    cron: "30 13 * * 1-7",
+    msg: {
+      title: "Battlegrounds",
+      desc: "Battlegrounds is starting now. Do your daily 3 battlegrounds to get rewards.",
+      color: "#F8C300",
+    },
+  },
+  {
+    cron: "30 17 * * 1-7",
+    msg: {
+      title: "Battlegrounds",
+      desc: "Battlegrounds is starting now. Do your daily 3 battlegrounds to get rewards.",
+      color: "#F8C300",
+    },
+  },
 
-  // // Zone events
+  // Zone events
 
-  // // Haunted Carriage
-  // {
-  //   cron: "0 20 7 * * 2,6",
-  //   msg: {
-  //     title: "Haunted Carriage",
-  //     desc: "Haunted Carriage starting in 10 mins in Ashwold Cemetery",
-  //     color: "#00D166",
-  //   },
-  // },
-  // {
-  //   cron: "0 25 7 * * 2,6",
-  //   msg: {
-  //     title: "Haunted Carriage",
-  //     desc: "Haunted Carriage starting in 5 mins in Ashwold Cemetery",
-  //     color: "#00D166",
-  //   },
-  // },
-  // {
-  //   cron: "0 30 7 * * 2,6",
-  //   msg: {
-  //     title: "Haunted Carriage",
-  //     desc: "Haunted Carriage started in Ashwold Cemetery",
-  //     color: "#00D166",
-  //   },
-  // },
-  // {
-  //   cron: "0 50 15 * * 2,6",
-  //   msg: {
-  //     title: "Haunted Carriage",
-  //     desc: "Haunted Carriage starting in 10 mins in Ashwold Cemetery",
-  //     color: "#00D166",
-  //   },
-  // },
-  // {
-  //   cron: "0 55 15 * * 2,6",
-  //   msg: {
-  //     title: "Haunted Carriage",
-  //     desc: "Haunted Carriage starting in 5 mins in Ashwold Cemetery",
-  //     color: "#00D166",
-  //   },
-  // },
-  // {
-  //   cron: "0 0 16 * * 2,6",
-  //   msg: {
-  //     title: "Haunted Carriage",
-  //     desc: "Haunted Carriage starting now in Ashwold Cemetery",
-  //     color: "#00D166",
-  //   },
-  // },
-  // {
-  //   cron: "0 20 17 * * 2,6",
-  //   msg: {
-  //     title: "Haunted Carriage",
-  //     desc: "Haunted Carriage starting in 10 mins in Ashwold Cemetery",
-  //     color: "#00D166",
-  //   },
-  // },
-  // {
-  //   cron: "0 25 17 * * 2,6",
-  //   msg: {
-  //     title: "Haunted Carriage",
-  //     desc: "Haunted Carriage starting in 5 mins in Ashwold Cemetery",
-  //     color: "#00D166",
-  //   },
-  // },
-  // {
-  //   cron: "0 30 17 * * 2,6",
-  //   msg: {
-  //     title: "Haunted Carriage",
-  //     desc: "Haunted Carriage starting now in Ashwold Cemetery",
-  //     color: "#00D166",
-  //   },
-  // },
+  // Haunted Carriage
+  {
+    cron: "20 7 * * 2,6",
+    msg: {
+      title: "Haunted Carriage",
+      desc: "Haunted Carriage starting in 10 mins in Ashwold Cemetery",
+      color: "#00D166",
+    },
+  },
+  {
+    cron: "25 7 * * 2,6",
+    msg: {
+      title: "Haunted Carriage",
+      desc: "Haunted Carriage starting in 5 mins in Ashwold Cemetery",
+      color: "#00D166",
+    },
+  },
+  {
+    cron: "30 7 * * 2,6",
+    msg: {
+      title: "Haunted Carriage",
+      desc: "Haunted Carriage started in Ashwold Cemetery",
+      color: "#00D166",
+    },
+  },
+  {
+    cron: "50 15 * * 2,6",
+    msg: {
+      title: "Haunted Carriage",
+      desc: "Haunted Carriage starting in 10 mins in Ashwold Cemetery",
+      color: "#00D166",
+    },
+  },
+  {
+    cron: "55 15 * * 2,6",
+    msg: {
+      title: "Haunted Carriage",
+      desc: "Haunted Carriage starting in 5 mins in Ashwold Cemetery",
+      color: "#00D166",
+    },
+  },
+  {
+    cron: "0 16 * * 2,6",
+    msg: {
+      title: "Haunted Carriage",
+      desc: "Haunted Carriage starting now in Ashwold Cemetery",
+      color: "#00D166",
+    },
+  },
+  {
+    cron: "20 17 * * 2,6",
+    msg: {
+      title: "Haunted Carriage",
+      desc: "Haunted Carriage starting in 10 mins in Ashwold Cemetery",
+      color: "#00D166",
+    },
+  },
+  {
+    cron: "25 17 * * 2,6",
+    msg: {
+      title: "Haunted Carriage",
+      desc: "Haunted Carriage starting in 5 mins in Ashwold Cemetery",
+      color: "#00D166",
+    },
+  },
+  {
+    cron: "30 17 * * 2,6",
+    msg: {
+      title: "Haunted Carriage",
+      desc: "Haunted Carriage starting now in Ashwold Cemetery",
+      color: "#00D166",
+    },
+  },
 
-  // // Ancient Arena
-  // {
-  //   cron: "0 50 16 * * 2,4,6,7",
-  //   msg: {
-  //     title: "Ancient Arena",
-  //     desc: "Ancient Arena starting in 10 mins. Show your might and get amazing rewards.",
-  //     color: "#00D166",
-  //   },
-  // },
-  // {
-  //   cron: "0 55 16 * * 2,4,6,7",
-  //   msg: {
-  //     title: "Ancient Arena",
-  //     desc: "Ancient Arena starting in 5 mins. Show your might and get amazing rewards.",
-  //     color: "#00D166",
-  //   },
-  // },
-  // {
-  //   cron: "0 0 17 * * 2,4,6,7",
-  //   msg: {
-  //     title: "Ancient Arena",
-  //     desc: "Ancient Arena starting now. Show your might and get amazing rewards.",
-  //     color: "#00D166",
-  //   },
-  // },
+  // Ancient Arena
+  {
+    cron: "50 16 * * 2,4,6,7",
+    msg: {
+      title: "Ancient Arena",
+      desc: "Ancient Arena starting in 10 mins. Show your might and get amazing rewards.",
+      color: "#00D166",
+    },
+  },
+  {
+    cron: "55 16 * * 2,4,6,7",
+    msg: {
+      title: "Ancient Arena",
+      desc: "Ancient Arena starting in 5 mins. Show your might and get amazing rewards.",
+      color: "#00D166",
+    },
+  },
+  {
+    cron: "0 17 * * 2,4,6,7",
+    msg: {
+      title: "Ancient Arena",
+      desc: "Ancient Arena starting now. Show your might and get amazing rewards.",
+      color: "#00D166",
+    },
+  },
 
-  // // Ancient Nightmare
-  // {
-  //   cron: "0 20 7 * * 3,5",
-  //   msg: {
-  //     title: "Ancient Nightmare",
-  //     desc: "Ancient Nightmare is starting now in 10 mins in Mist Valley in the middle of Mount Zavain",
-  //     color: "#00D166",
-  //   },
-  // },
-  // {
-  //   cron: "0 25 7 * * 3,5",
-  //   msg: {
-  //     title: "Ancient Nightmare",
-  //     desc: "Ancient Nightmare is starting now in 5 mins in Mist Valley in the middle of Mount Zavain",
-  //     color: "#00D166",
-  //   },
-  // },
-  // {
-  //   cron: "0 30 7 * * 3,5",
-  //   msg: {
-  //     title: "Ancient Nightmare",
-  //     desc: "Ancient Nightmare is starting now in Mist Valley in the middle of Mount Zavain",
-  //     color: "#00D166",
-  //   },
-  // },
-  // {
-  //   cron: "0 20 17 * * 3,5",
-  //   msg: {
-  //     title: "Ancient Nightmare",
-  //     desc: "Ancient Nightmare is starting now in 10 mins in Mist Valley in the middle of Mount Zavain",
-  //     color: "#00D166",
-  //   },
-  // },
-  // {
-  //   cron: "0 25 17 * * 3,5",
-  //   msg: {
-  //     title: "Ancient Nightmare",
-  //     desc: "Ancient Nightmare is starting now in 5 mins in Mist Valley in the middle of Mount Zavain",
-  //     color: "#00D166",
-  //   },
-  // },
-  // {
-  //   cron: "0 30 17 * * 3,5",
-  //   msg: {
-  //     title: "Ancient Nightmare",
-  //     desc: "Ancient Nightmare is starting now in Mist Valley in the middle of Mount Zavain",
-  //     color: "#00D166",
-  //   },
-  // },
-  // {
-  //   cron: "0 50 15 * * 3,5",
-  //   msg: {
-  //     title: "Ancient Nightmare",
-  //     desc: "Ancient Nightmare is starting now in 10 mins in Mist Valley in the middle of Mount Zavain",
-  //     color: "#00D166",
-  //   },
-  // },
-  // {
-  //   cron: "0 55 15 * * 3,5",
-  //   msg: {
-  //     title: "Ancient Nightmare",
-  //     desc: "Ancient Nightmare is starting now in 5 mins in Mist Valley in the middle of Mount Zavain",
-  //     color: "#00D166",
-  //   },
-  // },
-  // {
-  //   cron: "0 0 16 * * 3,5",
-  //   msg: {
-  //     title: "Ancient Nightmare",
-  //     desc: "Ancient Nightmare is starting now in Mist Valley in the middle of Mount Zavain",
-  //     color: "#00D166",
-  //   },
-  // },
+  // Ancient Nightmare
+  {
+    cron: "20 7 * * 3,5",
+    msg: {
+      title: "Ancient Nightmare",
+      desc: "Ancient Nightmare is starting now in 10 mins in Mist Valley in the middle of Mount Zavain",
+      color: "#00D166",
+    },
+  },
+  {
+    cron: "25 7 * * 3,5",
+    msg: {
+      title: "Ancient Nightmare",
+      desc: "Ancient Nightmare is starting now in 5 mins in Mist Valley in the middle of Mount Zavain",
+      color: "#00D166",
+    },
+  },
+  {
+    cron: "30 7 * * 3,5",
+    msg: {
+      title: "Ancient Nightmare",
+      desc: "Ancient Nightmare is starting now in Mist Valley in the middle of Mount Zavain",
+      color: "#00D166",
+    },
+  },
+  {
+    cron: "20 17 * * 3,5",
+    msg: {
+      title: "Ancient Nightmare",
+      desc: "Ancient Nightmare is starting now in 10 mins in Mist Valley in the middle of Mount Zavain",
+      color: "#00D166",
+    },
+  },
+  {
+    cron: "25 17 * * 3,5",
+    msg: {
+      title: "Ancient Nightmare",
+      desc: "Ancient Nightmare is starting now in 5 mins in Mist Valley in the middle of Mount Zavain",
+      color: "#00D166",
+    },
+  },
+  {
+    cron: "30 17 * * 3,5",
+    msg: {
+      title: "Ancient Nightmare",
+      desc: "Ancient Nightmare is starting now in Mist Valley in the middle of Mount Zavain",
+      color: "#00D166",
+    },
+  },
+  {
+    cron: "50 15 * * 3,5",
+    msg: {
+      title: "Ancient Nightmare",
+      desc: "Ancient Nightmare is starting now in 10 mins in Mist Valley in the middle of Mount Zavain",
+      color: "#00D166",
+    },
+  },
+  {
+    cron: "55 15 * * 3,5",
+    msg: {
+      title: "Ancient Nightmare",
+      desc: "Ancient Nightmare is starting now in 5 mins in Mist Valley in the middle of Mount Zavain",
+      color: "#00D166",
+    },
+  },
+  {
+    cron: "0 16 * * 3,5",
+    msg: {
+      title: "Ancient Nightmare",
+      desc: "Ancient Nightmare is starting now in Mist Valley in the middle of Mount Zavain",
+      color: "#00D166",
+    },
+  },
 ];
 
 client.on("ready", async () => {
