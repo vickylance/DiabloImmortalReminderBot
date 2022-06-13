78346a3f2d8a587b27a172569daa077bcc4fe88b
Vignesh S <vigneshs6993@gmail.com>
2022-06-13 10:49:13 +0530

    Removed everything except daily reset

--- index.js
+++ index.js
@@ -18,179 +18,179 @@ const eventJson = [
     },
   },
 
-  // Shadow lottery
-  {
-    cron: "0 30 7 * * 1-6",
-    msg: {
-      title: "Shadow Lottery",
-      desc: "Shadow lottery is up. Apply for Lottery to get Akiba's signets",
-      color: "#A652BB",
-    },
-  },
-  {
-    cron: "0 30 13 * * 1-6",
-    msg: {
-      title: "Shadow Lottery",
-      desc: "Shadow lottery is up. Apply for Lottery to get Akiba's signets",
-      color: "#A652BB",
-    },
-  },
-  {
-    cron: "0 30 16 * * 1-6",
-    msg: {
-      title: "Shadow Lottery",
-      desc: "Shadow lottery is up. Apply for Lottery to get Akiba's signets",
-      color: "#A652BB",
-    },
-  },
+  // // Shadow lottery
+  // {
+  //   cron: "0 30 7 * * 1-6",
+  //   msg: {
+  //     title: "Shadow Lottery",
+  //     desc: "Shadow lottery is up. Apply for Lottery to get Akiba's signets",
+  //     color: "#A652BB",
+  //   },
+  // },
+  // {
+  //   cron: "0 30 13 * * 1-6",
+  //   msg: {
+  //     title: "Shadow Lottery",
+  //     desc: "Shadow lottery is up. Apply for Lottery to get Akiba's signets",
+  //     color: "#A652BB",
+  //   },
+  // },
+  // {
+  //   cron: "0 30 16 * * 1-6",
+  //   msg: {
+  //     title: "Shadow Lottery",
+  //     desc: "Shadow lottery is up. Apply for Lottery to get Akiba's signets",
+  //     color: "#A652BB",
+  //   },
+  // },
 
-  // Shadow contracts
-  {
-    cron: "0 30 22 * * 1-6",
-    msg: {
-      title: "Shadow Contracts",
-      desc: "1 Shadow Contract is reset. Do your Contract.",
-      color: "#A652BB",
-    },
-  },
-  {
-    cron: "0 30 22 * * 7",
-    msg: {
-      title: "Shadow Contracts",
-      desc: "3 Shadow Contracts are reset. Do your Contracts.",
-      color: "#A652BB",
-    },
-  },
+  // // Shadow contracts
+  // {
+  //   cron: "0 30 22 * * 1-6",
+  //   msg: {
+  //     title: "Shadow Contracts",
+  //     desc: "1 Shadow Contract is reset. Do your Contract.",
+  //     color: "#A652BB",
+  //   },
+  // },
+  // {
+  //   cron: "0 30 22 * * 7",
+  //   msg: {
+  //     title: "Shadow Contracts",
+  //     desc: "3 Shadow Contracts are reset. Do your Contracts.",
+  //     color: "#A652BB",
+  //   },
+  // },
 
-  // Vault raid
-  {
-    cron: "0 30 7 * * 1-7",
-    msg: {
-      title: "Vault Raid",
-      desc: "Immortal's Vault is open. Lets go for their Vault raid.",
-      color: "#A652BB",
-    },
-  },
-  {
-    cron: "0 30 14 * * 1-7",
-    msg: {
-      title: "Vault Raid",
-      desc: "Immortal's Vault is open. Lets go for their Vault raid.",
-      color: "#A652BB",
-    },
-  },
+  // // Vault raid
+  // {
+  //   cron: "0 30 7 * * 1-7",
+  //   msg: {
+  //     title: "Vault Raid",
+  //     desc: "Immortal's Vault is open. Lets go for their Vault raid.",
+  //     color: "#A652BB",
+  //   },
+  // },
+  // {
+  //   cron: "0 30 14 * * 1-7",
+  //   msg: {
+  //     title: "Vault Raid",
+  //     desc: "Immortal's Vault is open. Lets go for their Vault raid.",
+  //     color: "#A652BB",
+  //   },
+  // },
 
-  // Shadow assembly
-  {
-    cron: "0 30 13 * * 1-6",
-    msg: {
-      title: "Shadow Assembly",
-      desc: "Shadow Assembly is commencing now. Everyone get your Akiba's blessings. And your 15 Aspirant keys.",
-      color: "#A652BB",
-    },
-  },
+  // // Shadow assembly
+  // {
+  //   cron: "0 30 13 * * 1-6",
+  //   msg: {
+  //     title: "Shadow Assembly",
+  //     desc: "Shadow Assembly is commencing now. Everyone get your Akiba's blessings. And your 15 Aspirant keys.",
+  //     color: "#A652BB",
+  //   },
+  // },
 
-  // Rite of exile
-  {
-    cron: "0 15 15 * * 7",
-    msg: {
-      title: "Rite of Exile",
-      desc: "Rite of exile is commencing now in 15 mins. Need all hands on Deck needed.",
-      color: "#A652BB",
-    },
-  },
-  {
-    cron: "0 20 15 * * 7",
-    msg: {
-      title: "Rite of Exile",
-      desc: "Rite of exile is commencing now in 10 mins. Need all hands on Deck needed.",
-      color: "#A652BB",
-    },
-  },
-  {
-    cron: "0 25 15 * * 7",
-    msg: {
-      title: "Rite of Exile",
-      desc: "Rite of exile is commencing now in 5 mins. Need all hands on Deck needed.",
-      color: "#A652BB",
-    },
-  },
-  {
-    cron: "0 30 15 * * 7",
-    msg: {
-      title: "Rite of Exile",
-      desc: "Rite of exile is commencing now. Good luck bois.",
-      color: "#A652BB",
-    },
-  },
+  // // Rite of exile
+  // {
+  //   cron: "0 15 15 * * 7",
+  //   msg: {
+  //     title: "Rite of Exile",
+  //     desc: "Rite of exile is commencing now in 15 mins. Need all hands on Deck needed.",
+  //     color: "#A652BB",
+  //   },
+  // },
+  // {
+  //   cron: "0 20 15 * * 7",
+  //   msg: {
+  //     title: "Rite of Exile",
+  //     desc: "Rite of exile is commencing now in 10 mins. Need all hands on Deck needed.",
+  //     color: "#A652BB",
+  //   },
+  // },
+  // {
+  //   cron: "0 25 15 * * 7",
+  //   msg: {
+  //     title: "Rite of Exile",
+  //     desc: "Rite of exile is commencing now in 5 mins. Need all hands on Deck needed.",
+  //     color: "#A652BB",
+  //   },
+  // },
+  // {
+  //   cron: "0 30 15 * * 7",
+  //   msg: {
+  //     title: "Rite of Exile",
+  //     desc: "Rite of exile is commencing now. Good luck bois.",
+  //     color: "#A652BB",
+  //   },
+  // },
 
-  // Shadow War
-  {
-    cron: "0 15 13 * * 4,6",
-    msg: {
-      title: "Shadow War",
-      desc: "Shadow War is commencing now in 15 mins. Need all hands on Deck needed.",
-      color: "#A652BB",
-    },
-  },
-  {
-    cron: "0 20 13 * * 4,6",
-    msg: {
-      title: "Shadow War",
-      desc: "Shadow War is commencing now in 10 mins. Need all hands on Deck needed.",
-      color: "#A652BB",
-    },
-  },
-  {
-    cron: "0 25 13 * * 4,6",
-    msg: {
-      title: "Shadow War",
-      desc: "Shadow War is commencing now in 5 mins. Need all hands on Deck needed.",
-      color: "#A652BB",
-    },
-  },
-  {
-    cron: "0 30 13 * * 4,6",
-    msg: {
-      title: "Shadow War",
-      desc: "Shadow War is commencing now. Good luck bois.",
-      color: "#A652BB",
-    },
-  },
+  // // Shadow War
+  // {
+  //   cron: "0 15 13 * * 4,6",
+  //   msg: {
+  //     title: "Shadow War",
+  //     desc: "Shadow War is commencing now in 15 mins. Need all hands on Deck needed.",
+  //     color: "#A652BB",
+  //   },
+  // },
+  // {
+  //   cron: "0 20 13 * * 4,6",
+  //   msg: {
+  //     title: "Shadow War",
+  //     desc: "Shadow War is commencing now in 10 mins. Need all hands on Deck needed.",
+  //     color: "#A652BB",
+  //   },
+  // },
+  // {
+  //   cron: "0 25 13 * * 4,6",
+  //   msg: {
+  //     title: "Shadow War",
+  //     desc: "Shadow War is commencing now in 5 mins. Need all hands on Deck needed.",
+  //     color: "#A652BB",
+  //   },
+  // },
+  // {
+  //   cron: "0 30 13 * * 4,6",
+  //   msg: {
+  //     title: "Shadow War",
+  //     desc: "Shadow War is commencing now. Good luck bois.",
+  //     color: "#A652BB",
+  //   },
+  // },
 
-  // Battlegrounds
-  {
-    cron: "0 30 3 * * 1-7",
-    msg: {
-      title: "Battlegrounds",
-      desc: "Battlegrounds is starting now. Do your daily 3 battlegrounds to get rewards.",
-      color: "#F8C300",
-    },
-  },
-  {
-    cron: "0 30 7 * * 1-7",
-    msg: {
-      title: "Battlegrounds",
-      desc: "Battlegrounds is starting now. Do your daily 3 battlegrounds to get rewards.",
-      color: "#F8C300",
-    },
-  },
-  {
-    cron: "0 30 13 * * 1-7",
-    msg: {
-      title: "Battlegrounds",
-      desc: "Battlegrounds is starting now. Do your daily 3 battlegrounds to get rewards.",
-      color: "#F8C300",
-    },
-  },
-  {
-    cron: "0 30 17 * * 1-7",
-    msg: {
-      title: "Battlegrounds",
-      desc: "Battlegrounds is starting now. Do your daily 3 battlegrounds to get rewards.",
-      color: "#F8C300",
-    },
-  },
+  // // Battlegrounds
+  // {
+  //   cron: "0 30 3 * * 1-7",
+  //   msg: {
+  //     title: "Battlegrounds",
+  //     desc: "Battlegrounds is starting now. Do your daily 3 battlegrounds to get rewards.",
+  //     color: "#F8C300",
+  //   },
+  // },
+  // {
+  //   cron: "0 30 7 * * 1-7",
+  //   msg: {
+  //     title: "Battlegrounds",
+  //     desc: "Battlegrounds is starting now. Do your daily 3 battlegrounds to get rewards.",
+  //     color: "#F8C300",
+  //   },
+  // },
+  // {
+  //   cron: "0 30 13 * * 1-7",
+  //   msg: {
+  //     title: "Battlegrounds",
+  //     desc: "Battlegrounds is starting now. Do your daily 3 battlegrounds to get rewards.",
+  //     color: "#F8C300",
+  //   },
+  // },
+  // {
+  //   cron: "0 30 17 * * 1-7",
+  //   msg: {
+  //     title: "Battlegrounds",
+  //     desc: "Battlegrounds is starting now. Do your daily 3 battlegrounds to get rewards.",
+  //     color: "#F8C300",
+  //   },
+  // },
 
   // // Zone events
 
@@ -418,7 +418,7 @@ client.on("ready", async () => {
     };
 
     console.log(memoryUsage);
-  }, 1000 * 60);
+  }, 1000 * 10);
 });
 
 client.login(botToken);
